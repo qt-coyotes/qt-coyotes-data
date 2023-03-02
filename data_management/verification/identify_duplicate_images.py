1eb5a3233232042589119a123820c71b40ef743f
Steven Tang <18170455+steventango@users.noreply.github.com>
2023-03-02 00:47:26 -0700

    Identify duplicate images

--- data_management/verification/identify_duplicate_images.py
+++ data_management/verification/identify_duplicate_images.py
@@ -1,29 +1,42 @@
 import cv2
 import glob
+import json
 import os
+from tqdm import tqdm
 
-os.environ["PYTHONHASHSEED"] = "0"
+DUPLICATE_JSON_PATH = "data/processed/CHIL/duplicate_images.json"
+GLOB_PATTERNS = [
+    "data/raw/CHIL/**/*.jpg",
+    "data/raw/CHIL_earlier/**/*.jpg"
+]
 
 
-def hashes(glob_pattern):
-    dictionary = {}
-    for filename in glob.glob(glob_pattern, recursive=True):
+def identify_duplicate_images(filenames):
+    os.environ["PYTHONHASHSEED"] = "0"
+    hash_filenames = {}
+    for filename in tqdm(filenames):
         image = cv2.imread(filename)
         h = hash(image.data.tobytes())
-        if h in dictionary:
-            print(
-                f"Warning: {filename} has same hash as {dictionary[h]}"
-            )
-        dictionary[h] = filename
-    return set(dictionary.keys())
+        if h in hash_filenames:
+            hash_filenames[h].append(filename)
+        else:
+            hash_filenames[h] = [filename]
+    duplicates = sorted(filter(
+        lambda filenames: len(filenames) > 1, hash_filenames.values()
+    ))
+    return duplicates
 
 
 def main():
-    chil = hashes("data/raw/CHIL/**/*.jpg")
-    print(len(chil))
-    chil_earlier = hashes("data/raw/CHIL-earlier/**/*.JPG")
-    print(len(chil_earlier))
-    assert len(chil.intersection(chil_earlier)) == 0
+    filenames = []
+    for glob_pattern in GLOB_PATTERNS:
+        filenames.extend(glob.glob(glob_pattern, recursive=True))
+    duplicates = identify_duplicate_images(filenames)
+    with open(DUPLICATE_JSON_PATH, "w") as f:
+        json.dump(duplicates, f, indent=4)
+    print(f"Found {len(duplicates)} duplicate images.")
+    print(f"See: {DUPLICATE_JSON_PATH}")
+    assert len(duplicates) == 0
 
 
 if __name__ == "__main__":
